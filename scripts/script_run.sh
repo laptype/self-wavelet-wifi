eef4f663ffba7df205cea39fdb4a8db4ddee2887
laptop <bolan_xjtu@163.com>
2023-01-07 18:53:20 +0800

    change

--- scripts/script_run.sh
+++ scripts/script_run.sh
@@ -33,7 +33,8 @@ datasource_path=$8
 tab=$9
 
 echo "========================${dataset_name}-${backbone_name}-${strategy_name}-TRAIN========================"
-CUDA_VISIBLE_DEVICES=${cuda} ${python} /home/lanbo/wifi_wavelet/main.py --dataset_name ${dataset_name} --gpu_device ${cuda} \
+CUDA_VISIBLE_DEVICES=1,2 ${python} -m torch.distributed.launch --nproc_per_node=2 --use_env /home/lanbo/wifi_wavelet/main.py \
+--dataset_name ${dataset_name} --gpu_device ${cuda} \
 --backbone_name ${backbone_name} --head_name ${head_name} --strategy_name ${strategy_name} \
 --train_batch_size ${train_batch_size} --eval_batch_size ${eval_batch_size} --num_epoch ${num_epoch} \
 --opt_method ${opt_method} --lr_rate ${lr_rate} --weight_decay ${weight_decay} \
--- training.py
+++ training.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 
 
 def train(config: TrainConfig):
-    if config.gpu_device is not None:
-        os.environ["CUDA_VISIBLE_DEVICES"] = config.gpu_device
+    # if config.gpu_device is not None:
+    #     os.environ["CUDA_VISIBLE_DEVICES"] = config.gpu_device
 
     train_dataset, eval_dataset = init_util.init_dataset(config.dataset_name, config.datasource_path)
     strategy = init_util.init_strategy(config.backbone_name,
